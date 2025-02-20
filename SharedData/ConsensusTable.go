94c1b34dcbe11629f07ca6e47b700a557ab4523d
Elias Drøpping <eliasdr@outlook.com>
2025-02-20 14:46:45 +0100

    Removed code

--- SharedData/ConsensusTable.go
+++ SharedData/ConsensusTable.go
@@ -1,17 +1,1 @@
 package sharedData
-import (
-	"root/elevator"
-	"root/elevio"
-	)
-
-type LocalData struct {
-	CabRequests [elevio.NUM_FLOORS]bool
-	state elevator.Elevator
-	FloorRequests [elevio.NUM_FLOORS][elevio.NUM_BUTTONS]bool
-}	
-
-
-type SharedData struct {
-		hallRequests [elevio.NUM_FLOORS][elevio.NUM_BUTTONS]bool
-		
-}
