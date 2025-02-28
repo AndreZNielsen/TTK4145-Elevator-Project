c4862bd8bb3bff6f1081ec363ea7ee74340a50a7
elias-drp <eliasdr@outlook.com>
2025-02-28 12:43:58 +0100

    La til noen kommentarer/tanker i sharedHallRequests.go

--- SharedData/sharedHallRequests.go
+++ SharedData/sharedHallRequests.go
@@ -10,3 +10,10 @@ import (
  var sharedHallRequests [4][2]bool // om buttonEvent eller update skal sharedHallRequests oppdateres
  //sharedHallRequests er input i assigner.go og setAllLights() (skal skru på/av hallLys)
  //output fra assigner.go skal oppdatere elevator.requests 
+
+//Vi må lage en update kanal-variabel/kanal som varsler hver gang det sendes eller mottas en TCP-melding 
+
+//er det bedre/mulig å gjøre buttonEvent->sendTCPmessage til en update sånn at sharedHallRequests bare har ett input?
+//Funksjonen som henter data til sharedHallRequests må hente data fra både når buttonEvent skjer(lokalt)
+//og fra TCP-meldings-datastrukturen får en oppdatering. 
+
