8fea425d7f98c18c51803b861a9a6a8e4645a155
J0npry <148270418+J0npry@users.noreply.github.com>
2025-02-28 17:41:39 +0100

    Update transmitter.go

--- utility/transmitter.go
+++ utility/transmitter.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-var conn_lift1 net.Conn
+var encoder *gob.Encoder
 var sendMu sync.Mutex // Mutex to protect the sending of data
 
 type Elevator_data struct {
@@ -18,7 +18,9 @@ type Elevator_data struct {
 	CabRequests []bool
 	HallRequests [][2]bool
 }
-
+func InitEncoder(conn net.Conn) {
+	encoder = gob.NewEncoder(conn)
+}
 func Start_tcp_call(port string, ip string) {
 	var err error
 	conn_lift1, err = net.Dial("tcp", ip+":"+port)
@@ -26,14 +28,15 @@ func Start_tcp_call(port string, ip string) {
 		fmt.Println("Error connecting to pc:", ip, err)
 		time.Sleep(5 * time.Second)
 		Start_tcp_call(port, ip) // Retry connection
+	} else {
+		InitEncoder(conn net.Conn)
 	}
+
 }
 
 func Send_Elevator_data(data Elevator_data) {
 	sendMu.Lock() // Locking before sending
 	defer sendMu.Unlock() // Ensure to unlock after sending
-
-	encoder := gob.NewEncoder(conn_lift1)
 	err := encoder.Encode("elevator_data") // Type ID
 	if err != nil {
 		fmt.Println("Encoding error:", err)
@@ -50,7 +53,6 @@ func Send_update(update [3]int) {
 	sendMu.Lock() // Locking before sending
 	defer sendMu.Unlock() // Ensure to unlock after sending
 
-	encoder := gob.NewEncoder(conn_lift1)
 	err := encoder.Encode("int") // Type ID
 	if err != nil {
 		fmt.Println("Encoding error:", err)
