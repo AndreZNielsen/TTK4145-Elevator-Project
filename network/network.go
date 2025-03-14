85585f7325151e9ae2bbf17b1f1796bdae973795
J0npry <jonaslp12345@gmail.com>
2025-03-14 09:55:09 +0100

    Update network.go

--- network/network.go
+++ network/network.go
@@ -6,30 +6,28 @@ import(
 	"root/sharedData"
 	"root/config"
 	
-	"sort"
 	"fmt"
 	
 
 )
 
 func Start_network(receiver chan<- [3]int,disconnected chan<- string){
-	var counter int
+
 
 	for _, id := range config.PossibleIDs{
 		if id == config.Elevator_id {
-			counter +=1 
 			continue // Local elevator not needed 
 		}
 
 
-		if counter%2 == 0 {
+		if indexOfElevatorID(config.Elevator_id)< indexOfElevatorID(id) {// the elavator with the lowest index will dial 
 
 		sharedData.RemoteElevatorConnections[id] = transmitter.Start_tcp_call(portGenerateor(config.Elevator_id,id),config.Elevatoip[id],id,disconnected)	
 		}else{
 
 		sharedData.RemoteElevatorConnections[id] = reciver.Start_tcp_listen(portGenerateor(config.Elevator_id,id),id)
 		}
-		counter +=1 
+
 		
 	}
 
@@ -41,18 +39,16 @@ func Start_network(receiver chan<- [3]int,disconnected chan<- string){
 }
 
 func Network_reconnector(receiver chan<- [3]int,disconnected chan<- string, needReconnecting string){
-	var counter int
-		counter = 0
+
 
 	for _, id := range config.PossibleIDs{
 		if id == config.Elevator_id {
-			counter +=1 
 			continue // Local elevator not needed 
 		}
 
 
 
-		if counter%2 == 0 && needReconnecting == id{
+		if indexOfElevatorID(config.Elevator_id)< indexOfElevatorID(id) && needReconnecting == id{
 
 		sharedData.RemoteElevatorConnections[id] = transmitter.Start_tcp_call(portGenerateor(config.Elevator_id,id),config.Elevatoip[id],id,disconnected)	
 		go reciver.Recive(receiver,id,disconnected)
@@ -63,25 +59,32 @@ func Network_reconnector(receiver chan<- [3]int,disconnected chan<- string, need
 		go reciver.Recive(receiver,id,disconnected)
 
 		}
-		counter +=1 
 		
 	}
 	}
 
 
 func portGenerateor(localID, targetID string) string {
-	// Combine the two IDs in a deterministic order
-	ids := []string{localID, targetID}
-	sort.Strings(ids) // ensures the order is consistent regardless of input order
-	combined := ids[0] + ids[1]
+	var port int 
+	localIndex := indexOfElevatorID(localID)
+	targetIndex := indexOfElevatorID(targetID)
+	if localIndex < targetIndex{
+		port = 8000 + localIndex
+	}else {
+		port = 8000 + targetIndex
 
-	// makes a hash
-	var hash int
-	for _, ch := range combined {
-		hash += int(ch)
 	}
 
-	//we choose 8000 as the base to make it in typical port range
-	port := 8000 + (hash % 1000)
+
 	return fmt.Sprintf("%d", port)
+}
+
+func indexOfElevatorID(target string) int {
+    
+	for i, v := range config.PossibleIDs {
+        if v == target {
+            return i
+        }
+    }
+ 	return -1 // if not in array 
 }
