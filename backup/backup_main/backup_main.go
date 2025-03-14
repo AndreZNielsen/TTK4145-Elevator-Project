9a83489a985569d8e88436d38588e3d157ad352b
J0npry <jonaslp12345@gmail.com>
2025-03-14 22:44:42 +0100

    jobber med backup

---
+++ backup/backup_main/backup_main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+	"time"
+	
+)
+
+
+
+func main(){
+	fmt.Println("yo")
+	time.Sleep(time.Second*5)
+}
---
+++ backup/test.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"os/exec"
+)
+
+
+func main(){
+	Start_backup()
+}
+func Start_backup(){
+	psCommand := "Start-Process powershell -ArgumentList \"-NoExit\", \"-Command\", \"cd './backup_main'; go run backup_main.go\""
+
+	// Start PowerShell and execute the command
+	cmd := exec.Command("powershell.exe", "-Command", psCommand)
+	// Start the backup program 
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting PowerShell:", err)
+		return
+	}
+}
