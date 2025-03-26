d5062313c4e08fd40326199ad92cde6c0e3cd9dc
elias-drp <eliasdr@outlook.com>
2025-03-26 18:06:39 +0100

    Update config.go

--- config/config.go
+++ config/config.go
@@ -3,28 +3,14 @@ package config
 const Num_floors = 4
 
 var Elevator_id = "B"
-
-
 var PossibleIDs = []string{"A","B"}
-
-
 var LocalElevatorServerPort = "localhost:12346"
-
-
-
 var RemoteIDs = RemoveElement(PossibleIDs, Elevator_id)
-
 var Elevators_ip = map[string]string{
 	"A": "10.22.92.31",
-
 	"B": "localhost",
-
-
     //"C": "10.100.23.32",
-
 }
-
-
 
 func RemoveElement(slice []string, element string) []string {
     copiedSlice := make([]string, len(slice))
