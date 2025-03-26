816e74f7b75830083b04b445d066c2ef115f8a12
elias-drp <eliasdr@outlook.com>
2025-03-26 18:22:16 +0100

    This is the last purge of empty space

--- network/alivetimer.go
+++ network/alivetimer.go
@@ -2,16 +2,15 @@ package network
 
 import(
 	"time"
-
 	"root/config"
 )
+
 var (
 	timer = make(map[string]*time.Timer)
 	ini = make(map[string]bool)
 )
 
 func InitAliveTimer(){
-
 	for _, id := range config.RemoteIDs{
 		ini[id]=false
 	}
@@ -19,18 +18,14 @@ func InitAliveTimer(){
 
 
 func StartAliveTimer(elvatorDead chan string , id string) {
-
 	timer[id] = time.NewTimer(10 * time.Second)
 	ini[id] = true
-
 	<-timer[id].C
 	elvatorDead <- id
-	
 }
 	
 
 func ResetAliveTimer(id string){
-
 	if(ini[id]){
 		timer[id].Reset(10 * time.Second) // Reset the timer when the elvator is alive
 	}
@@ -39,7 +34,5 @@ func ResetAliveTimer(id string){
 
 func StopAliveTimer(id string){
 	timer[id].Stop()
-
 	ini[id] = false
-
 }
