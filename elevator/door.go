c19d67a25e460db4892c50d59f2e3e605acc36a1
elias-drp <eliasdr@outlook.com>
2025-03-26 18:14:10 +0100

    Purge

--- elevator/door.go
+++ elevator/door.go
@@ -5,16 +5,12 @@ import (
 	
 )
 
-
 func DoorOpen(elevator *Elevator) {
 	StartTimer()
 	elevio.SetDoorOpenLamp(true)
 	elevator.behaviour = Behaviour_door_open
-
 	StopStuckTimer(elevator)
-
 }
-
 
 func DoorClose(elevator *Elevator) {
 	StopTimer()
